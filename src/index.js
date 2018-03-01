8c041ed9d11b17e42b640f9507331c35e317883a
zinukov <36642204+zinukov@users.noreply.github.com>
2018-03-01 20:56:39 +0300

    sec

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-// PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
    if (currency > 10000) {
        return {error: " You are rich, my friend! We don't have so much coins for exchange");
